19a090450add7736e90f6f72ff8e7b95df160606
ajang <63917062+mfnurdiansyah@users.noreply.github.com>
2020-06-15 20:02:47 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function tampilkandua(){
+  document.getElementById("kotak3").style.visibility = "visible";
+  document.getElementById("kotak2").style.visibility = "hidden";
+}
+function tampilkantiga(){
+  document.getElementById("kotak4").style.visibility = "visible";
+  document.getElementById("kotak3").style.visibility = "hidden";
+}
+function tampilkanempat(){
+  document.getElementById("kotak2").style.visibility = "visible";
+  document.getElementById("kotak4").style.visibility = "hidden";
+}
+function ya(){
+  document.getElementById("kata").style.visibility = "visible";
+  document.getElementById("kata2").style.visibility = "hidden";
+  document.getElementById("kotak1").style.visibility = "hidden";
+  document.getElementById("kotak2").style.visibility = "hidden";
+  document.getElementById("kotak3").style.visibility = "hidden";
+  document.getElementById("kotak4").style.visibility = "hidden";
+}
